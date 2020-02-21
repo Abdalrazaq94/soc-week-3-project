84bd70446dcc9ddb9938b19ffb2898874989343c
abd#94 <abdalrazaqaltaih@gmail.com>
2020-02-21 22:32:45 +0100

    replace

--- challenges/repeat-characters.js
+++ challenges/repeat-characters.js
@@ -3,7 +3,15 @@ debugger; // step through loading & testing
 
 // reapeat each character in a string, without changing the order
 function repeatCharacters(str, repetitions) {
-
+  if( repetition >= 1){
+    return str.repeat();
+  }else if (repetition === 0){
+    return "";
+} else if (repetition === NaN){
+  return 'repetitions must be a number'
+}else if (repetition === string){
+  return 'repetitions must be a number' 
+}
 }
 
 // declare and evaluate test cases for repeatCharacters
@@ -30,14 +38,15 @@ function repeatCharactersHandler() {
   debugger; // step through user actions
 
   // read & process user input
-
+  const userText = prompt ('enter your text');
+  const numRepeats = ('enter your number')
   const numRepeatsIsANumber = typeof numRepeats === 'number';
   console.assert(numRepeatsIsANumber, 'cast numRepeats to type "number"');
 
   // execute core logic
-
+const result = repeatCharacters(userText, numRepeat.trm();)
   // display result to user
-
+alert(result)
   // log action for developer
   console.log('\n-- repeatCharacters --');
   // user inputs
